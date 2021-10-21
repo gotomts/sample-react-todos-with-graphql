2f7f2e51a870ea72ba15277f1c8c64bb4e4d2833
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-10-21 09:08:39 +0900

    feat: //go generateディレクティブ追加

--- server/graph/schema.resolvers.go
+++ server/graph/schema.resolvers.go
@@ -1,7 +1,6 @@
 package graph
 
-// This file will be automatically regenerated based on the schema, any resolver implementations
-// will be copied through when generating and any unknown code will be moved to the end.
+//go:generate go run github.com/99designs/gqlgen
 
 import (
 	"context"
