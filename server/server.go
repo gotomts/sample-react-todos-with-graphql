9a1d03b6485a253df90b9aaa7f03e2cd9591c897
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-10-22 00:30:12 +0900

    feat: cors設定用にchiとrs/corsを使ったサーバーに変更

--- server/server.go
+++ server/server.go
@@ -1,29 +1,46 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
 	"github.com/gotomts/sample-react-todos-with-graphql/server/graph"
 	"github.com/gotomts/sample-react-todos-with-graphql/server/graph/generated"
+	"github.com/rs/cors"
 )
 
-const defaultPort = "4000"
+const defaultPort = "8080"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+	router := chi.NewRouter()
+	port := defaultPort
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:"+port, "http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv.AddTransport(&transport.Websocket{
+			Upgrader: websocket.Upgrader{
+					CheckOrigin: func(r *http.Request) bool {
+						return r.Host == "example.org"
+					},
+					ReadBufferSize:  1024,
+					WriteBufferSize: 1024,
+			},
+	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		panic(err)
+	}
 }
