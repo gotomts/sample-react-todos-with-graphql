df09eb6106380f702049c4a2213fdf44889cc4e1
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-10-23 23:33:53 +0900

    feat: entのgenerateコマンドによる自動生成

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
@@ -9,6 +10,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
 	"github.com/gorilla/websocket"
+	"github.com/gotomts/sample-react-todos-with-graphql/server/ent"
 	"github.com/gotomts/sample-react-todos-with-graphql/server/graph"
 	"github.com/gotomts/sample-react-todos-with-graphql/server/graph/generated"
 	"github.com/rs/cors"
@@ -17,24 +19,34 @@ import (
 const defaultPort = "8080"
 
 func main() {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	// オートマイグレーションツールを実行する
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
 	router := chi.NewRouter()
 	port := defaultPort
 
 	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:"+port, "http://localhost:3000"},
+		AllowedOrigins:   []string{"http://localhost:" + port, "http://localhost:3000"},
 		AllowCredentials: true,
 		Debug:            true,
 	}).Handler)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 	srv.AddTransport(&transport.Websocket{
-			Upgrader: websocket.Upgrader{
-					CheckOrigin: func(r *http.Request) bool {
-						return r.Host == "example.org"
-					},
-					ReadBufferSize:  1024,
-					WriteBufferSize: 1024,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return r.Host == "example.org"
 			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
 	})
 
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
